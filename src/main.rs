18cb86e982dfb158ffee842adbe058c593ce294c
DameHetfield9803 <you@example.com>
2024-01-31 04:32:40 +0800

    fsdfdsfedsf

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,28 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+#[post("/index")]
+async fn index_post(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn man_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey how are you?")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(index_post)
+            .route("/hey", web::get().to(man_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
